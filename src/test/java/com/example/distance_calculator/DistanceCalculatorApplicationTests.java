11b9fb9d0196d10049e0f8f2c741c1433b7bac01
olishiz <zhuen1101@hotmail.com>
2025-01-15 11:21:30 +0800

    Initialize repo with Spring Framework + Java

---
+++ src/test/java/com/example/distance_calculator/DistanceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distance_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
